467da71f7e277d0310a166b73375b996068d7a53
paramila <paramila@unirioja.es>
2026-06-16 23:05:32 +0200

    Subida de swagger

--- src/main/java/io/swagger/client/model/SolicitudResponse.java
+++ src/main/java/io/swagger/client/model/SolicitudResponse.java
@@ -23,7 +23,7 @@ import java.util.Arrays;
 public class SolicitudResponse {
 
   @Override
-  public boolean equals(java.lang.Object o) {
+  public boolean equals(Object o) {
     if (this == o) {
       return true;
     }
@@ -52,7 +52,7 @@ public class SolicitudResponse {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(java.lang.Object o) {
+  private String toIndentedString(Object o) {
     if (o == null) {
       return "null";
     }
